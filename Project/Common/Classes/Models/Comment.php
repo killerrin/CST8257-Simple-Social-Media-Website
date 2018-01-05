d47f6e32749dfb4c562423034549ad4e48706a67
Patrick Gingras <ging0044@algonquinlive.com>
2018-01-05 15:24:03 -0500

    Added missing $Date property to Comment Class

--- Project/Common/Classes/Models/Comment.php
+++ Project/Common/Classes/Models/Comment.php
@@ -14,12 +14,14 @@ class Comment
     public $Author_Id;
     public $Picture_Id;
     public $Comment_Text;
+    public $Date;
 
-    public function __construct($comment_Id, $author_Id, $picture_Id, $comment_Text) {
+    public function __construct($comment_Id, $author_Id, $picture_Id, $comment_Text, $date) {
         $this->Comment_Id = $comment_Id;
         $this->Author_Id = $author_Id;
         $this->Picture_Id = $picture_Id;
         $this->Comment_Text = $comment_Text;
+        $this->Date = $date;
     }
 
     public function GetUserAuthor(DBUserRepository $repo) {
