5fffad9ad359ec128b9c1a18cf421242f86b2dd1
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-01-04 20:04:37 -0500

    Added classes to IncludeAll

--- Project/Common/IncludeAll.php
+++ Project/Common/IncludeAll.php
@@ -3,10 +3,18 @@
 // ================================================================================
 
 // ================ Models ================
+include_once("Classes/Models/Accessibility.php");
+include_once("Classes/Models/FriendshipStatus.php");
+include_once("Classes/Models/User.php");
+include_once("Classes/Models/Friendship.php");
+include_once("Classes/Models/Album.php");
+include_once("Classes/Models/Picture.php");
+include_once("Classes/Models/Comment.php");
 
 // ================ Database ==============
 include_once("Classes/DataAccess/DBManager.php");
 include_once("Classes/DataAccess/DBGenericRepository.php");
+include_once("Classes/DataAccess/Repositories/DBUserRepository.php");
 
 // ================ Other =================
 include_once("Classes/Image/Picture.php");
