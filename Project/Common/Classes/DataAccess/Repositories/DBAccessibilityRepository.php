7293a9d86b4a361167438ccfddd25ea78411d4f4
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-01-04 23:40:05 -0500

    Updated AccessabilityRepo

--- Project/Common/Classes/DataAccess/Repositories/DBAccessibilityRepository.php
+++ Project/Common/Classes/DataAccess/Repositories/DBAccessibilityRepository.php
@@ -24,6 +24,7 @@ class DBAccessibilityRepository extends DBGenericRepository
     }
 
     private function rowToObject($row) {
+        return new Accessibility($row[0], $row[1]);
     }
 
     public function getAll()
@@ -32,21 +33,31 @@ class DBAccessibilityRepository extends DBGenericRepository
         return $this->parseQuery($result);
     }
 
+    //  Get Accessibility by Accessibility_Code
     function getID($key) {
+        $result = $this->dbManager->queryByFilter($this->tableName, "Accessibility_Code", $key);
         return $this->parseQuery($result);
     }
 
+    //  Return true if success, else false
+    function insert(Accessibility $item) {
         $query = "INSERT INTO $this->tableName
+                  VALUES('$item->Accessibility_Code', '$item->Description')";
         return $this->dbManager->queryCustom($query);
     }
 
     // Return True of Success, False if failed
+    function update(Accessibility $item) {
         $query = "UPDATE $this->tableName
+                  SET Description = '$item->Description'
+                  WHERE Accessibility_Code = '$item->Accessibility_Code'";
         return $this->dbManager->queryCustom($query);
     }
 
     // Return True of Success, False if failed
+    function delete(Accessibility $item) {
         $query = "DELETE FROM $this->tableName
+                  WHERE Accessibility_Code = '$item->Accessibility_Code'";
         return $this->dbManager->queryCustom($query);
     }
 }
