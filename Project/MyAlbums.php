2a573d6c80aca7e35a5eedecfea2801e07928d4e
Patrick Gingras <ging0044@algonquinlive.com>
2018-01-04 22:28:08 -0500

    Removed a tiny bit of whitespace

--- Project/MyAlbums.php
+++ Project/MyAlbums.php
@@ -3,7 +3,7 @@
 
 <?php
 // If user is logged in, assign Student object to $LoggedInUser, otherwise redirect to login and die (self-executing function)
-$LoggedInUser = isset($_SESSION["LoggedInUser"]) ? $_SESSION["LoggedInUser"] : (function() { header("Location: Login.php?returnUrl=".urlencode($_SERVER['REQUEST_URI'])); die();})();
+$LoggedInUser = isset($_SESSION["LoggedInUser"])?$_SESSION["LoggedInUser"]:(function(){header("Location: Login.php?returnUrl=".urlencode($_SERVER['REQUEST_URI']));die();})();
 ?>
 
 <div class="container">
