dc3d9c5470600494c8524e478879eb41d969b9ab
Patrick Gingras <ging0044@algonquinlive.com>
2018-01-04 19:53:10 -0500

    Added Picture model, added Date_Updated as parameter in Album constructor

---
+++ Project/Common/Classes/Models/Picture.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patrick
+ * Date: 2018-01-04
+ * Time: 19:47
+ */
+
+class Picture
+{
+    public $Picture_Id;
+    public $Album_Id;
+    public $FileName;
+    public $Title;
+    public $Description;
+    public $Date_Added;
+
+    public function __construct($pictureId, $albumId, $fileName, $title, $description, $dateAdded)
+    {
+        $this->Picture_Id = $pictureId;
+        $this->Album_Id = $albumId;
+        $this->FileName = $fileName;
+        $this->Title = $title;
+        $this->Description = $description;
+        $this->Date_Added = $dateAdded;
+    }
+
+    public function GetAlbum() {
+        return;
+    }
+}
