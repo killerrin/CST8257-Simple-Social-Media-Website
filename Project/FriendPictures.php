b79d57e1edd0885659b3e407691ac97ffe8c09fe
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-01-07 14:23:55 -0500

    Repo and Data Scaffholding

--- Project/FriendPictures.php
+++ Project/FriendPictures.php
@@ -4,10 +4,22 @@
 <?php
 // If user is logged in, assign Student object to $LoggedInUser, otherwise redirect to login and die (self-executing function)
 $LoggedInUser = isset($_SESSION["LoggedInUser"]) ? $_SESSION["LoggedInUser"] : (function() { header("Location: Login.php?returnUrl=".urlencode($_SERVER['REQUEST_URI'])); die();})();
+
+$dbManager = new DBManager();
+$userRepo = new DBUserRepository($dbManager);
+$albumManager = new DBAlbumRepository($dbManager);
+$pictureRepo = new DBPictureRepository($dbManager);
+$friendshipRepo = new DBFriendshipRepository($dbManager);
+$commentRepo = new DBCommentRepository($dbManager);
+
+$dbManager->connect();
+$friend = $userRepo->getID($_GET["id"]);
+$friendAlbums = $albumRepo->getAllForUserAccessibility($friend->User_Id, "shared");
+$dbManager->close();
 ?>
 
-    <div class="container">
+<div class="container">
 
-    </div>
+</div>
 
 <?php include "Common/Footer.php"; ?>
