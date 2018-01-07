483c5a0704e0786e30573605f8c740e234b28341
Andrew Godfroy <andrew.godfroy@killerrin.com>
2018-01-07 18:19:50 -0500

    Added a Prefill option for AddFriend

--- Project/AddFriend.php
+++ Project/AddFriend.php
@@ -43,36 +43,40 @@ if (!empty($_POST)) {
 }
 ?>
 
-    <div class="container">
-        <h1>Add Friend</h1>
+<div class="container">
+    <h1>Add Friend</h1>
+    <p>
+        Welcome
+        <strong>
+            <?php echo $LoggedInUser->Name; ?>!
+        </strong>(not you? change user
+        <a href="Logout.php">here</a>)
+    </p>
+    <p>Enter the ID of the user you would like to be friends with:</p>
+    <br />
+    <?php if (isset($errorMessage)): ?>
+    <div class="alert alert-danger">
         <p>
-            Welcome
-            <strong>
-                <?php echo $LoggedInUser->Name; ?>!
-            </strong>(not you? change user
-            <a href="Logout.php">here</a>)
+            <span class="glyphicon glyphicon-thumbs-down"></span><?php echo $errorMessage; ?>
         </p>
-        <p>Enter the ID of the user you would like to be friends with:</p>
-        <br />
-        <?php if (isset($errorMessage)): ?>
-        <div class="alert alert-danger">
-            <p><span class="glyphicon glyphicon-thumbs-down"></span> <?php echo $errorMessage; ?></p>
-        </div>
-        <?php endif; ?>
-        <?php if (isset($success)): ?>
-        <div class="alert alert-success">
-            <p><span class="glyphicon glyphicon-thumbs-up"></span> Your friend request has been sent to <?php echo "$friend->Name (ID: $friend->User_Id). Once $friend->Name accepts your request, you and $friend->Name will be friends and be able to view each other's shared albums."; ?></p>
-        </div>
-        <?php endif; ?>
-        <form action="AddFriend.php" method="post" class="form-inline center-block">
-            <div class="form-group">
-                <label for="friendId">ID:</label>
-                <input type="text" class="form-control" name="friendId" id="friendId" placeholder="Friend ID" />
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Send Friend Request" />
-            </div>
-        </form>
     </div>
+    <?php endif; ?>
+    <?php if (isset($success)): ?>
+    <div class="alert alert-success">
+        <p>
+            <span class="glyphicon glyphicon-thumbs-up"></span>Your friend request has been sent to <?php echo "$friend->Name (ID: $friend->User_Id). Once $friend->Name accepts your request, you and $friend->Name will be friends and be able to view each other's shared albums."; ?>
+        </p>
+    </div>
+    <?php endif; ?>
+    <form action="AddFriend.php" method="post" class="form-inline center-block">
+        <div class="form-group">
+            <label for="friendId">ID:</label>
+            <input type="text" class="form-control" name="friendId" id="friendId" placeholder="Friend ID" value="<?php echo $_GET["prefillUserID"];?>" />
+        </div>
+        <div class="form-group">
+            <input type="submit" class="btn btn-primary" value="Send Friend Request" />
+        </div>
+    </form>
+</div>
 
 <?php include "Common/Footer.php"; ?>
--- Project/FriendPictures.php
+++ Project/FriendPictures.php
@@ -9,16 +9,28 @@ $dbManager = new DBManager();
 $userRepo = new DBUserRepository($dbManager);
 $albumManager = new DBAlbumRepository($dbManager);
 $pictureRepo = new DBPictureRepository($dbManager);
+$friendshipRepo = new DBFriendshipRepository($dbManager);
 $commentRepo = new DBCommentRepository($dbManager);
 
 $dbManager->connect();
 $friend = $userRepo->getID($_GET["id"]);
 $friendAlbums = $albumRepo->getAllForUserAccessibility($friend->User_Id, "shared");
+$areFriends = Friendship::AreUsersFriends($friendshipRepo, $LoggedInUser, $friend);
 $dbManager->close();
 ?>
 
 <div class="container">
+    <?php if($areFriends) :?>
 
+    <?php else : ?>
+    <h1>Access Denied</h1>
+    <p>You do not have access to this album.</p>
+    <p>
+        <a href="AddFriend.php?prefillUserID=<?php echo $friend->User_Id; ?>">Request this person be added to your friends list</a>in order to continue
+    </p>
+    <?php endif;?>
 </div>
+
+
 
 <?php include "Common/Footer.php"; ?>
